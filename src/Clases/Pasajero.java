ceb9dfb82776a969996d58ea9eb7308023cbc43c
Cesar Da Silva <113476857+DaSilvaCesar@users.noreply.github.com>
2024-10-02 20:51:47 -0300

    Se sube clase Pasajerosform

--- src/Clases/Pasajero.java
+++ src/Clases/Pasajero.java
@@ -1,3 +1,5 @@
+
+
 package Clases;
 
 import java.util.Objects;
---
+++ src/PasajeroFormulario.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class PasajeroFormulario {
+    private JTextField apellidoTxt;
+    private JLabel apellidoLbl;
+    private JLabel nombreLbl;
+    private JLabel documentoLbl;
+    private JLabel telefonoLbl;
+    private JLabel correoLbl;
+    private JTextField nombreTxt;
+    private JTextField documentoTxt;
+    private JTextField telefonoTxt;
+    private JTextField correoTxt;
+    private JButton eliminarButton;
+    private JButton agregarButton;
+    private JButton modificarButton;
+    private JTable table1;
+}
